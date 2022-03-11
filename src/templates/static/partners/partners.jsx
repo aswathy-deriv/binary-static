5bb3976aa55686ee5126ee1382f40c42da5743a3
aswathy-deriv <aswathy@deriv.com>
2022-03-11 09:18:32 +0400

    changed double quotes to single quotes

--- src/templates/static/partners/partners.jsx
+++ src/templates/static/partners/partners.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Column = ({ url, target, image, header, text }) => (
-    <div className="gr-4 gr-12-m gr-no-gutter center-text-m">
-        <div className="gr-6 gr-padding-10 gr-centered-m">
+    <div className='gr-4 gr-12-m gr-no-gutter center-text-m'>
+        <div className='gr-6 gr-padding-10 gr-centered-m'>
             {url && (
                 <a
                     href={`${url}`}
@@ -10,13 +10,13 @@ const Column = ({ url, target, image, header, text }) => (
                     target={target || undefined}
                 >
                     <img
-                        className="responsive"
+                        className='responsive'
                         src={it.url_for(`images/pages/partners/${image}.svg`)}
                     />
                 </a>
             )}
         </div>
-        <div className="gr-12">
+        <div className='gr-12'>
             <h4>
                 <a
                     href={`${url}`}
@@ -33,8 +33,8 @@ const Column = ({ url, target, image, header, text }) => (
 
 const Partners = () => (
     <React.Fragment>
-        <div className="container">
-            <div className="static_full">
+        <div className='container'>
+            <div className='static_full'>
                 <h1>{it.L('Partners')}</h1>
                 <p>
                     {it.L(
@@ -44,17 +44,17 @@ const Partners = () => (
                 </p>
             </div>
 
-            <div className="gr-row gr-clear">
+            <div className='gr-row gr-clear'>
                 <Column
                     url={it.url_for('payment-agent')}
-                    image="payment-agents"
+                    image='payment-agents'
                     header={it.L('Payment Agent')}
                     text={it.L('Apply to become a Payment Agent')}
                 />
                 <Column
-                    url="https://developers.binary.com"
-                    target="_blank"
-                    image="api"
+                    url='https://developers.binary.com'
+                    target='_blank'
+                    image='api'
                     header={it.L('API')}
                     text={it.L(
                         'Develop your own trading application with our APIs.'
@@ -63,7 +63,7 @@ const Partners = () => (
 
                 <Column
                     url={it.url_for('open-source-projects')}
-                    image="open-source-venture"
+                    image='open-source-venture'
                     header={it.L('Open Source')}
                     text={it.L(
                         "Contribute to [_1]'s open-source projects.",
@@ -72,17 +72,17 @@ const Partners = () => (
                 />
             </div>
 
-            <div className="gr-row gr-clear">
+            <div className='gr-row gr-clear'>
                 <Column
-                    url="https://academy.binary.com/en/contributors/"
-                    image="contributors"
-                    target="_blank"
+                    url='https://academy.binary.com/en/contributors/'
+                    image='contributors'
+                    target='_blank'
                     header={it.L('Contributors')}
                     text={it.L('Apply to become a contributor')}
                 />
                 <Column
                     url={it.url_for('security-testing')}
-                    image="tester"
+                    image='tester'
                     header={it.L('Security Testing')}
                     text={it.L(
                         'Earn money for any verifiable errors that you find.'
